a10f95e63a2cc7564ad9f7f243c9ed311db0080c
Rahul Gunkar <rgunkar@yahoo.in>
2016-07-08 16:47:41 +0530

    interfacing onboard temperature sensor using ADC

--- main.c
+++ main.c
@@ -2,106 +2,79 @@
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/gpio.h"
 #include "driverlib/debug.h"
-#include "driverlib/pwm.h"
-#include "driverlib/pin_map.h"
-#include "inc/hw_gpio.h"
-#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/adc.h"
 
-//define pwm frequency
-#define PWM_FREQUENCY 55
+/*To store the data read from the adc fifo It should be equal to the sequencer used
+ * since we are using sequencer 1 with the depth of 4, the next few variables are
+ * for avg adc temp, celcious,farenhiet value*/
+uint32_t ui32ADC0Value[4];
+volatile uint32_t ui32TempAvg;
+volatile uint32_t ui32TempValueC;
+volatile uint32_t ui32TempValueF;
 
-/*
- since pwm frequency is 55 and a time time period of 18.2ms and we are gonna devide it by 1000
-  so we get the precision of 1.82us..now 1.82*83=1.5ms...since servo is at zero angle when supplided
- */
-volatile uint32_t ui32Load;
-volatile uint32_t ui32PWMClock;
-volatile uint32_t ui8Adjust=83;
-
-/*the following function will set the clock with 40MHz  and the second line of the code will divide the
- * pwm frequency by 64
- * ROM version is used to reduce the code size
- */
-
-void SetClock()
+void ClockConfigure()
 {
 	SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
-	SysCtlPWMClockSet(SYSCTL_PWMDIV_64);
-}
-/*enable port D,F for pwm and smd onboard leds*/
-
-void ConfigPins()
-{
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
-	GPIOPinTypePWM(GPIO_PORTD_BASE,GPIO_PIN_0);
-	GPIOPinConfigure(GPIO_PD0_M1PWM0);
 }
 
-
-void UnlockPortF()
+void Adc0Seq1Configure()
 {
-	HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY; //addres to unlock
-	HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= 0x01;   //commit resgister to change
-	HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;		// clear to make changes
-	GPIODirModeSet(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0, GPIO_DIR_MODE_IN);  //set PF4,0 as input internal pull
-	GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+	//enable periphera
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+	//configure adc 0 ,sequencer 1,trigger the processor with highset priority
+	ADCSequenceConfigure(ADC0_BASE, 1, ADC_TRIGGER_PROCESSOR, 0);
+	//sample sequencer 4 times
+	ADCSequenceStepConfigure(ADC0_BASE, 1, 0, ADC_CTL_TS);
+	ADCSequenceStepConfigure(ADC0_BASE, 1, 1, ADC_CTL_TS);
+	ADCSequenceStepConfigure(ADC0_BASE, 1, 2, ADC_CTL_TS);
+	/*The final sequencer step requires a couple of extra settings. Sample the temperature
+	sensor (ADC_CTL_TS) and configure the interrupt flag (ADC_CTL_IE) to be set
+	when the sample is done. Tell the ADC logic that this is the last conversion on
+	sequencer	1 (ADC_CTL_END).*/
+	ADCSequenceStepConfigure(ADC0_BASE,1,3,ADC_CTL_TS|ADC_CTL_IE|ADC_CTL_END);
+	//enable adc sequencer one
+	ADCSequenceEnable(ADC0_BASE, 1);
+	
 }
 
-void SetPwmTimePeriod()
+void StartAdc()
 {
-	/*pwm clock is system clock/64 divide the pwm clock by 55 so to get the value of the loaad register-1
-	 * configure pwm module 1 generator 0 as a down counter and load the count value
-	 */
-
-	ui32PWMClock = SysCtlClockGet() / 64;
-	ui32Load = (ui32PWMClock / PWM_FREQUENCY) - 1;
-	PWMGenConfigure(PWM1_BASE, PWM_GEN_0, PWM_GEN_MODE_DOWN);
-	PWMGenPeriodSet(PWM1_BASE, PWM_GEN_0, ui32Load);
-	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui8Adjust * ui32Load / 1000);
-	PWMOutputState(PWM1_BASE, PWM_OUT_0_BIT, true);
-	PWMGenEnable(PWM1_BASE, PWM_GEN_0);
-
+	//clear the interrupt flag in the beginning
+	ADCIntClear(ADC0_BASE, 1);
+	//Trigger the adc directly from the software as it can be triggered from many other divies
+	ADCProcessorTrigger(ADC0_BASE, 1);
+	//wait for the conversion to complete
+	while(!ADCIntStatus(ADC0_BASE, 1, false))
+	{
+	}
 }
 
 
-void ControlServo()
+void TempCalculations()
 {
-	if(GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4)==0x00)
-	{
-	ui8Adjust--;
-	if (ui8Adjust < 56)
-	{
-	ui8Adjust = 56;
-	}
-	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui8Adjust * ui32Load / 1000);
-	}
-	if(GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_0)==0x00)
-	{
-	ui8Adjust++;
-	if (ui8Adjust > 111)
-	{
-	ui8Adjust = 111;
-	}
-	PWMPulseWidthSet(PWM1_BASE, PWM_OUT_0, ui8Adjust * ui32Load / 1000);
-	}
-	SysCtlDelay(100000);
+	//read the adc value from the fifo
+	ADCSequenceDataGet(ADC0_BASE, 1, ui32ADC0Value);
+	//take the average of the four samples
+	ui32TempAvg = (ui32ADC0Value[0] + ui32ADC0Value[1] + ui32ADC0Value[2] + ui32ADC0Value[3] + 2)/4;
+	//calculate the temperature in celcius by the following formula
+	//TEMP = 147.5 – ((75 * (VREFP – VREFN) * ADCVALUE) / 4096)
+	//now VREFP-VRFEN=Vdd or 3.3v,we will multiple by 10 to get precision  and then multyply by 75
+	ui32TempValueC = (1475 - ((2475 * ui32TempAvg)) / 4096)/10;
+	ui32TempValueF = ((ui32TempValueC * 9) + 160) / 5;
+
 }
 
 
 int main()
 {
-	 SetClock();
-	 ConfigPins();
-	 UnlockPortF();
-	 SetPwmTimePeriod();
-
-	while(1){
-				ControlServo();
+	Adc0Seq1Configure();
+	while(1)
+	{
+		StartAdc();
+		TempCalculations();
 
 	}
 }
+
